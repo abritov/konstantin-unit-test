69717e86d33e09221242dfde38c9e4c910996f52
envy124 <pkgsail.dev@gmail.com>
2021-04-05 09:17:59 +0300

    fix FileFilterRepository

--- src/main/java/io/mdfitumi/unittest/repositories/FileFilterRepository.java
+++ src/main/java/io/mdfitumi/unittest/repositories/FileFilterRepository.java
@@ -3,10 +3,11 @@ package io.mdfitumi.unittest.repositories;
 import io.mdfitumi.unittest.models.FileObj;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public interface FileFilterRepository {
+public interface FileFilterRepository extends JpaRepository<FileObj, Long> {
     Page<FileObj> findByFilter(boolean checkDeleted, boolean deleted, boolean checkExt, String ext, boolean checkOwnerId, UUID ownerUuid, LocalDateTime beginDateTime, LocalDateTime endDateTime, Pageable pageable);
 }
